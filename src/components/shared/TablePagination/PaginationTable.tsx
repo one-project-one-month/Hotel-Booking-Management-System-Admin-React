24a9ef4a40246e3e08e6e95ff7e13a5224d169fa
shinnThantKyaw <shinthantkyaw1818@gmail.com>
2025-06-04 22:03:39 +0630

    fix:coupons-pagination

--- src/components/shared/TablePagination/PaginationTable.tsx
+++ src/components/shared/TablePagination/PaginationTable.tsx
@@ -8,36 +8,38 @@ import {
 } from "@/components/ui/pagination";
 import type { paginationType } from "@/utils/types/Pagination";
 
-
-
-
-
-const PaginationTable = ({prevClick,pages,currentPage,nextClick,pageClick}:paginationType) => {
+const PaginationTable = ({
+  prevClick,
+  pages,
+  currentPage,
+  nextClick,
+  pageClick,
+}: paginationType) => {
   return (
     <Pagination className="justify-end">
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious href="#" onClick={prevClick} />
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" onClick={prevClick} />
+        </PaginationItem>
+        {pages.map((p, index) => {
+          return (
+            <PaginationItem key={index}>
+              <PaginationLink
+                href="#"
+                isActive={currentPage === p}
+                onClick={() => pageClick(p)}
+              >
+                {p}
+              </PaginationLink>
             </PaginationItem>
-            {pages.map((p, index) => {
-              return (
-                <PaginationItem key={index}>
-                  <PaginationLink
-                    href="#"
-                    isActive={currentPage === p}
-                    onClick={() => pageClick(p)}
-                  >
-                    {p}
-                  </PaginationLink>
-                </PaginationItem>
-              );
-            })}
-            <PaginationItem>
-              <PaginationNext href="#" onClick={nextClick} />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-  )
-}
+          );
+        })}
+        <PaginationItem>
+          <PaginationNext href="#" onClick={nextClick} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
 
-export default PaginationTable
+export default PaginationTable;
